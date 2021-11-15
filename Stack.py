afa25dc7397cc581fc90c958d3ceec032b315837
leonrode <leon.rode13@gmail.com>
2021-11-14 21:46:17 -0500

    typed Stack

--- Stack.py
+++ Stack.py
@@ -1,15 +1,15 @@
 class Stack:
-    def __init__(self):
+    def __init__(self) -> None:
         self.stack = []
 
-    def push(self, value):
+    def push(self, value) -> None:
         self.stack.append(value)
 
-    def pop(self):
+    def pop(self) -> any:
         return self.stack.pop()
 
-    def peek(self):
+    def peek(self) -> any:
         return self.stack[len(self.stack) - 1]
 
-    def size(self):
+    def size(self) -> int:
         return len(self.stack)
